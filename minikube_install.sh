eb22432e15013b6e08e88627ebf3fd71ab30c170
harshalk91 <harshalk.91@gmail.com>
2019-04-19 17:03:09 +0000

    Added minikube installation config

--- minikube_install.sh
+++ minikube_install.sh
@@ -3,7 +3,7 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s htt
 
 chmod +x ./kubectl
 
-sudo mv ./kubectl /usr/local/bin/kubectl
+sudo mv ./kubectl /usr/bin/kubectl
 
 sudo yum install -y yum-utils \
   device-mapper-persistent-data \
@@ -19,3 +19,5 @@ sudo yum install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl start docker
 
 sudo systemctl enable docker
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/bin/minikube
