f7245a701892344595002e515059bdc2d2054fa8
Jombay <harshalk.91@gmail.com>
2019-04-20 18:01:21 +0530

    Modified main.tf and user-data

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "my-test-instance" {
   ami             = "ami-02e60be79e78fef21" 
-  instance_type   = "t2.micro"
+  instance_type   = "t2.medium"
   vpc_security_group_ids  = ["sg-01fe62c6f88a2ffa2"]
   subnet_id	  = "subnet-0c59f2b12acdcefd5"
   key_name 	  = "demo_mediawiki"
   associate_public_ip_address = true
-  user_data = "${file("../shared/user-data.txt")}"
+  user_data = "${file("user-data.txt")}"
 
   tags {
     Name = "test-instance"
